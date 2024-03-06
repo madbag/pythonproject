1c9bcf02c5decd7b96f9568ce9ee4626a447b133
Madhushree B <boylemadhushree@gmail.com>
2024-03-07 00:50:21 +0100

    initial commit

---
+++ analysis.py
@@ -0,0 +1,23 @@
+import csv
+
+def read_data():
+    data = []
+
+    with open('sales.csv','r') as sales_csv:
+        spreadsheet = csv.DictReader(sales_csv)
+        for row in spreadsheet:
+            data.append(row)
+    return data
+
+def run():
+    data = read_data()
+
+    sales = []
+    for row in data:
+        sale = int(row['sales'])
+        sales.append(sale)
+
+    total = sum(sales)
+    print('Total sale:{}'.format(total))
+
+run()
