0c4f2f4ea9224fc63d5f519c85693a374d6b55ef
Madhushree B <boylemadhushree@gmail.com>
2024-03-09 13:03:17 +0100

    Adding summary to sales and expenditure

--- analysis.py
+++ analysis.py
@@ -1,14 +1,20 @@
+# getting functionality of CSV module into the program
 import csv
 
 def read_data():
     data = []
-
-    with open('sales.csv','r') as sales_csv:
+    # # 1. Read the data from the spreadsheet
+    with open('sales.csv', 'r') as sales_csv:
+        # iterating rows as dictionaries.
+        # Rows = values and columns = keys
         spreadsheet = csv.DictReader(sales_csv)
+        # # 2. Collect all of the sales from each month into a single list
         for row in spreadsheet:
             data.append(row)
     return data
+print(read_data())
 
+# # 3. Output the total sales across all months
 def run():
     data = read_data()
 
@@ -19,5 +25,20 @@ def run():
 
     total = sum(sales)
     print('Total sale:{}'.format(total))
+    return total
+    run()
 
-run()
+# # 4. Summary of the results
+def summary():
+    data = read_data()
+
+    sales_Sum = run()
+    expenditure = []
+    for row in data:
+        exp = int(row['expenditure'])
+        expenditure.append(exp)
+
+    total = sum(expenditure)
+    print('Summary of the result for the year 2018: {} sales, {} expenditure'. format(sales_Sum, total))
+summary()
+
